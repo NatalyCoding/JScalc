830645b76c71feec18b5779ad37a1e8837defc46
Nataly Panakhyd <natalycoding@gmail.com>
2020-02-20 22:15:12 +0200

    feat: switch to next screen after basic data entered

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 const startButton = document.querySelector('.start-button'),
     firstScreen = document.querySelector('.first-screen'),
-    mainForm = document.querySelector('.main-form');
+    mainForm = document.querySelector('.main-form'),
+    formCalculate = document.querySelector('.form-calculate'),
+    endButton = document.querySelector('.end-button'),
+    total = document.querySelector('.total');
+
 
 function showElem(elem) {
     elem.style.display = 'block';
@@ -10,7 +14,20 @@ function hideElem(elem) {
     elem.style.display = 'none';
 }
 
+
 startButton.addEventListener('click', function() {
     showElem(mainForm);
     hideElem(firstScreen);
-})
+});
+
+endButton.addEventListener('click', function() {
+
+    for (const elem of formCalculate.elements) {
+        if (elem.tagName === 'FIELDSET') {
+            hideElem(elem);
+        }
+    }
+
+    showElem(total);
+
+});
