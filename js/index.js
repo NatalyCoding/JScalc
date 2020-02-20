0c2752d6a6bc2a3edb4ee9a954de92c75e449560
Nataly Panakhyd <natalycoding@gmail.com>
2020-02-20 22:34:42 +0200

    feat: show fast range block if checked

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ const startButton = document.querySelector('.start-button'),
     mainForm = document.querySelector('.main-form'),
     formCalculate = document.querySelector('.form-calculate'),
     endButton = document.querySelector('.end-button'),
-    total = document.querySelector('.total');
+    total = document.querySelector('.total'),
+    fastRange = document.querySelector('.fast-range');
 
 
 function showElem(elem) {
@@ -14,7 +15,12 @@ function hideElem(elem) {
     elem.style.display = 'none';
 }
 
-function handlerCallBackForm() {
+function handlerCallBackForm(event) {
+    const target = event.target;
+
+    if (target.classList.contains('want-faster')) {
+        target.checked ? showElem(fastRange) : hideElem(fastRange);
+    }
 
 };
 
